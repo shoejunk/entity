03ba6034119eb31faac7b680c80a49fde9e3a326
Joseph Shunk <joseph.shunk@gmail.com>
2023-09-28 01:28:50 -0700

    Progress on the entity-node system.

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,9 +1,26 @@
 import stk.entity;
+import stk.log;
 
 using namespace stk::entity;
+using namespace NStk::NLog;
 
 int main()
 {
+	class foo
+	{
+	public:
+		void go_foo()
+		{
+			Log("go foo!\n");
+		}
+	};
     node entity;
+	foo f;
+	entity.add_aspect(f);
+	foo* fp = entity.get_aspect<foo>();
+	if (fp != nullptr)
+	{
+		fp->go_foo();
+	}
     return 0;
 }
